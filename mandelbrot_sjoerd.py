9132e27fb941c6b2d98a2983cf788d81fbe158bc
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-10-28 12:09:22 +0100

    Implemente load and save mandelbrot set

--- mandelbrot_sjoerd.py
+++ mandelbrot_sjoerd.py
@@ -7,12 +7,12 @@ def mandelbrot_map(z, c):
     """Definition of the mandelbrot_map: z_n+1 = (z_n)^2 + c """
     return z**2 + c
 
-def mandelbrot(grid_size, iterations):
+def mandelbrot(grid_size, n_iterations):
     """Create mandelbrot set
 
     Args:
         grid_size -- length of grid in each direction
-        iterations -- number of iterations per mandelbrot number c
+        n_iterations -- number of n_iterations per mandelbrot number c
 
     Returns:
         x_mandelbrot - list of x coordinates of mandelbrot set
@@ -36,14 +36,14 @@ def mandelbrot(grid_size, iterations):
 
             # max size of mandelbrot set is 2
             z = 0
-            for _ in range(iterations):
+            for _ in range(n_iterations):
 
                 # mandelbrot formula z_n+1 = (z_n)^2 + c
                 z = z**2 + c
                 if abs(z) >= 2:
                     break
 
-            # if after iterations z < 2 --> member of mandelbrot set
+            # if after n_iterations z < 2 --> member of mandelbrot set
             if abs(z) < 2:
                 x_mandelbrot.append(x)
                 y_mandelbrot.append(y)
@@ -63,16 +63,46 @@ def plot_layout():
 
     return fig, ax
 
+def save_mandelbot(fname, x_mandelbrot, y_mandelbrot):
+    """Save mandelbrot set """
+    np.savetxt(fname + "_x", x_mandelbrot)
+    np.savetxt(fname + "_y", y_mandelbrot)
+
+    return
+
+def load_mandelbrot(fname):
+    """Loads mandelbrot set """
+    x_mandelbrot = np.loadtxt(fname + "_x")
+    y_mandelbrot = np.loadtxt(fname + "_y")
+
+    return x_mandelbrot, y_mandelbrot
+
 def main():
 
-    time_start = time.time()
+    grid_size = 2000
+    n_iterations = 50
+    fname = "results/mandelbrot_{}_{}".format(grid_size, n_iterations)
+    calculate_new_mandelbrot = True
 
-    # retrieve the mandelbrot set
-    x_mandelbrot, y_mandelbrot = mandelbrot(2000, 50)
+    if calculate_new_mandelbrot == True:
+        time_start = time.time()
 
-    print(time.time() - time_start)
+        # calculate the mandelbrot set
+        x_mandelbrot, y_mandelbrot = mandelbrot(grid_size, n_iterations)
 
-    print(len(x_mandelbrot))
+        # save mandelbrot set (only use this when calculating a new mandelbrot set)
+        save_mandelbot(fname, x_mandelbrot, y_mandelbrot)
+        print("Time to calculate mandelbrot set: {} s".format(time.time() - time_start))
+
+    else:
+
+        time_start = time.time()
+
+        # load mandelbrot set (only use this when not calculating a new mandelbrot set)
+        x_mandelbrot, y_mandelbrot = load_mandelbrot(fname)
+        print("Time to calculate mandelbrot set: {} s".format(time.time() - time_start))
+
+    # plot mandelbrot set
     fig, ax = plot_layout()
     plt.scatter(x_mandelbrot, y_mandelbrot)
     plt.show()
