7c4cf5c3bbb611fcb87bb476ad1c1e9a25acae9f
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-10-30 15:44:44 +0100

    added integration and matrix representation

--- mandelbrot_sjoerd.py
+++ mandelbrot_sjoerd.py
@@ -3,125 +3,127 @@ import matplotlib.pyplot as plt
 import time
 
 
-def mandelbrot(grid_size, n_iterations):
+def mandelbrot(dims, grid_size, mandel_max_iter):
     """Create mandelbrot set
 
     Args:
         grid_size -- length of grid in each direction
-        n_iterations -- number of n_iterations per mandelbrot number c
+        mandel_max_iter -- maximum number of iterations per candidate number c
 
     Returns:
-        x_mandelbrot - list of x coordinates of mandelbrot set
-        y_mandelbrot - list of y coordinates of mandelbrot set
+        mandelbrot_set -- matrix of number of iterations before escaping mandelbrot.
+                          Equal to mandel_max_iter if in mandelbrot set
     """
 
-    # save all calculated mandelbrot numbers to these lists
-    x_mandelbrot = []
-    y_mandelbrot = []
+    # create grid points to evaluate for mandelbrot set
+    x_grid = np.linspace(dims[0], dims[1], grid_size)
+    y_grid = np.linspace(dims[2], dims[3], grid_size)
 
-    # store the amount of iterations it takes for a number to escape the set
+    # create matrix to store number of iterations to escape the set
     # if it doesnt escape: iterations is set to 0
-    iterations_mandelbrot = []
-
-    # check these points for mandelbrot numbers
-    x_grid = np.linspace(-2, 0.6, grid_size)
-    y_grid = np.linspace(-1, 1, grid_size)
+    mandelbrot_set = np.zeros((grid_size, grid_size))
 
     # loop over all coordinates
-    for x in x_grid:
-        for y in y_grid:
+    for i, x in enumerate(x_grid):
+        for j, y in enumerate(y_grid):
 
             # create imaginary number c = a + bi
             c = complex(x, y)
 
             # max size of mandelbrot set is 2
             z = 0
-            i = 0
-            for i in range(n_iterations):
+            k = 0
+            while abs(z) <= 2 and k < mandel_max_iter:
 
                 # mandelbrot formula z_n+1 = (z_n)^2 + c
                 z = z**2 + c
-                if abs(z) >= 2:
-                    break
+                k += 1
 
             # save the number of iterations. 0 denotes mandelbrot set number
-            # if after n_iterations z < 2 --> member of mandelbrot set
-            if abs(z) < 2:
-                iterations_mandelbrot.append(0)
-            else:
-                iterations_mandelbrot.append(i + 1)
+            # if after mandel_max_iter z < 2 --> member of mandelbrot set
+            mandelbrot_set[j][i] = k - 1
 
-            x_mandelbrot.append(x)
-            y_mandelbrot.append(y)
-
-    return np.array(x_mandelbrot), np.array(y_mandelbrot), np.array(iterations_mandelbrot)
+    return mandelbrot_set
 
 
 def plot_layout():
     """Standard plot layout for figures """
     fig = plt.figure(facecolor='w')
     ax = fig.add_subplot(111, facecolor='#e8e8e8', axisbelow=True)
-    # ax.yaxis.set_tick_params(length=0)
-    # ax.xaxis.set_tick_params(length=0)
-    # ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-    # for spine in ('top', 'right', 'bottom', 'left'):
-    #     ax.spines[spine].set_visible(False)
 
     return fig, ax
 
-def save_mandelbot(fname, x_mandelbrot, y_mandelbrot, iterations_mandelbrot):
+def load_mandelbrot(fname):
+    """Load mandelbrot set """
+    mandelbrot_set = np.loadtxt(fname)
+
+    return mandelbrot_set
+
+def save_mandelbot(fname, mandelbrot_set):
     """Save mandelbrot set """
-    np.savetxt(fname + "_x", x_mandelbrot)
-    np.savetxt(fname + "_y", y_mandelbrot)
-    np.savetxt(fname + "_iter", iterations_mandelbrot)
+    np.savetxt(fname, mandelbrot_set)
 
     return
 
-def load_mandelbrot(fname):
-    """Loads mandelbrot set """
-    x_mandelbrot = np.loadtxt(fname + "_x")
-    y_mandelbrot = np.loadtxt(fname + "_y")
-    iterations_mandelbrot = np.loadtxt(fname + "_iter")
+def integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, mc_max_iter=10000):
+    """Integrate the mandelbrot set using Monte Carlo method """
 
-    return x_mandelbrot, y_mandelbrot, iterations_mandelbrot
+    hit = 0
+    miss = 0
+
+    print(np.amax(mandelbrot_set))
+
+    for i in range(mc_max_iter):
+        # throw a random number on the grid
+        x_rand = np.random.randint(grid_size)
+        y_rand = np.random.randint(grid_size)
+
+        if mandelbrot_set[y_rand][x_rand] == mandel_max_iter - 1:
+            hit += 1
+        else:
+            miss += 1
+
+    print(hit, miss)
+    # calculate total surface over which we integrate
+    surface = (dims[1] - dims[0]) * (dims[3] - dims[2])
+
+    # calculate are of mandelbrot
+    area = hit / (miss + hit) * (surface)
+
+    return area
 
 def main():
 
+    np.random.seed()
+
+    dims = [-2, 0.6, -1, 1]
     grid_size = 1000
-    n_iterations = 50
-    fname = "results/mandelbrot_{}_{}".format(grid_size, n_iterations)
+    mandel_max_iter = 100
+    fname = "results/mandelbrot_{}_{}".format(grid_size, mandel_max_iter)
 
     # set to True if you want to calculate a new mandelbrot set
-    # set to False if you want to load an existing mandelbrot set from "results/"
+    # set to False if you want to load an existing mandelbrot set from fname
     calculate_new_mandelbrot = False
 
     if calculate_new_mandelbrot == True:
         time_start = time.time()
 
-        # calculate the mandelbrot set
-        x_mandelbrot, y_mandelbrot, iterations_mandelbrot = mandelbrot(grid_size, n_iterations)
-
-        # save mandelbrot set (only use this when calculating a new mandelbrot set)
-        save_mandelbot(fname, x_mandelbrot, y_mandelbrot, iterations_mandelbrot)
+        # calculate the mandelbrot set and save to txt file
+        mandelbrot_set = mandelbrot(dims, grid_size, mandel_max_iter)
+        save_mandelbot(fname, mandelbrot_set)
         print("Time to calculate mandelbrot set: {} s".format(time.time() - time_start))
-
     else:
-        time_start = time.time()
 
         # load mandelbrot set (only use this when not calculating a new mandelbrot set)
-        x_mandelbrot, y_mandelbrot, iterations_mandelbrot = load_mandelbrot(fname)
-        print("Time to load mandelbrot set: {} s".format(time.time() - time_start))
+        mandelbrot_set = load_mandelbrot(fname)
 
-    # plot mandelbrot set (can take a while, because of the coloring of each coordinate)
+    # calculate integral of mandelbrot set using Monte Carlo
+    mandelbrot_area = integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter)
+    print("The integral of the mandelbrot set is {}".format(mandelbrot_area))
+
     fig, ax = plot_layout()
-    ax.set_xlim(-2, 0.5)
-    ax.set_ylim(-1, 1)
-    plt.scatter(x_mandelbrot, y_mandelbrot, c=iterations_mandelbrot)
-
-    # saving take a lot of time, so only do this when you really want to keep the image.
-    # Otherwise plt.show() is faster
-    # print("Saving figure...")
-    # plt.savefig("results/mandelbrot_{}_{}.png".format(grid_size, n_iterations), dpi=500)
+    plt.imshow(mandelbrot_set, extent=dims)
+    plt.savefig("results/mandelbrot_{}_{}.png".format(grid_size, mandel_max_iter), dpi=1000)
     plt.show()
     print("Done!")
 
