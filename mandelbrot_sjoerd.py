60f1ece47a0aadf1c9434f01128fa782ebc64ea2
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-11-01 14:03:02 +0100

    added antithetic variables for speed-up and accuracy

--- mandelbrot_sjoerd.py
+++ mandelbrot_sjoerd.py
@@ -29,6 +29,8 @@ def mandelbrot(dims, grid_size, mandel_max_iter):
 
     # loop over all coordinates
     for i, x in enumerate(x_grid):
+
+        # print("Progression: {:.2%}".format(i/len(x_grid)))
         for j, y in enumerate(y_grid):
 
             # create imaginary number c = a + bi
@@ -65,8 +67,9 @@ def save_mandelbot(fname, mandelbrot_set):
     """Save mandelbrot set """
     return np.savetxt(fname, mandelbrot_set)
 
-def integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, mc_max_iter=10000):
-    """Integrate the mandelbrot set using Monte Carlo method
+def integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, antithetic=False, mc_max_iter=10000):
+    """Integrate the mandelbrot set using Monte Carlo method. User can set antithetic to True
+    to use antithetic variables
 
     Args:
         mandelbrot_set : 2D numpy array
@@ -77,6 +80,8 @@ def integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, mc_ma
             length of grid in each direction
         mandel_max_iter : int
             maximum number of iterations per candidate number c
+        antithetic : boolean
+            use antithetic variate
         mc_max_iter : int
             number of random points to throw for integration
 
@@ -88,6 +93,10 @@ def integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, mc_ma
     hit = 0
     miss = 0
 
+    # if using antithetic, reduce the number of evals with a factor 2
+    if antithetic:
+        mc_max_iter = int(mc_max_iter / 2)
+
     for i in range(mc_max_iter):
 
         # throw a random number on the grid
@@ -98,6 +107,16 @@ def integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, mc_ma
         else:
             miss += 1
 
+        # get extra random number to evaluate when using antithetic method:
+        if antithetic:
+            x_rand_antithetic = 1 - x_rand
+            y_rand_antithetic = 1 - y_rand
+
+            if mandelbrot_set[y_rand_antithetic][x_rand_antithetic] == mandel_max_iter - 1:
+                hit += 1
+            else:
+                miss += 1
+
     # calculate total surface over which we integrate
     surface = (dims[1] - dims[0]) * (dims[3] - dims[2])
 
@@ -107,9 +126,12 @@ def integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, mc_ma
 def main():
 
     np.random.seed()
-    dims = [-2, 0.6, -1.1, 1.1]
-    grid_size = 256
-    mandel_max_iter = 256
+
+    antithetic = True               # use antithetic variables while integrating
+    dims = [-2, 0.6, -1.1, 1.1]     # dimensions of the search space [x1, x2, y1, y2]
+    grid_size = 1000                # amount of grid points in each dimension
+    mandel_max_iter = 256           # maximum of iterations for mandelbrot set
+
     fname = "results/mandelbrot_{}_{}".format(grid_size, mandel_max_iter)
 
     # set to True if you want to calculate a new mandelbrot set
@@ -128,7 +150,8 @@ def main():
         mandelbrot_set = load_mandelbrot(fname)
 
     # calculate integral of mandelbrot set using Monte Carlo
-    mandelbrot_area = integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, mc_max_iter=100000)
+    ## TODO: integrate multiple times and take the mean and stddev
+    mandelbrot_area = integrate_mandelbrot(mandelbrot_set, dims, grid_size, mandel_max_iter, antithetic=antithetic, mc_max_iter=100000)
     print("The integral of the mandelbrot set is {}".format(mandelbrot_area))
 
     # plot the mandelbrot set
@@ -137,7 +160,15 @@ def main():
     plt.imshow(mandelbrot_set, extent=dims)
     plt.savefig("results/mandelbrot_{}_{}.png".format(grid_size, mandel_max_iter), dpi=1000)
 
-########################################################################################
+    # save in true size
+    # plt.imsave("results/mandelbrot_{}_{}_true_size.png", arr=mandelbrot_set, format='png')
+
+    ###
+    # comment out next part if you only want to calculate the integral and don't want to see the
+    # convergence rate (takes relatively long to calculate)
+    ###
+    ########################################################################################
+
     # investigate the convergence
     mandelbrot_areas_conv = []
     for max_iter in range(mandel_max_iter):
@@ -148,7 +179,7 @@ def main():
         mandelbrot_set_conv = mandelbrot(dims, grid_size, max_iter)
 
         # integrate mandelbrot set using max_iter
-        surface = integrate_mandelbrot(mandelbrot_set_conv, dims, grid_size, max_iter)
+        surface = integrate_mandelbrot(mandelbrot_set_conv, dims, grid_size, max_iter, antithetic=True)
         mandelbrot_areas_conv.append(surface)
         print(surface)
 
@@ -163,6 +194,8 @@ def main():
     plt.xlabel("Iterations")
     plt.ylabel("Differences in surface")
     plt.savefig("results/mandelbrot_diffs_iter_{}_{}.png".format(grid_size, mandel_max_iter), dpi=1000)
+    ########################################################################################
+
     plt.show()
     print("Done!")
 
